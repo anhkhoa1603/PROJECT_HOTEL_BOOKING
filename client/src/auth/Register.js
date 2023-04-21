2359c243789bbfd6f0167aa245c5843f23e482ee
Khoa <anhkhoa162003@gmail.com>
2023-04-21 23:57:21 +0700

    [+] User Registration

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,5 +1,54 @@
-const Register = () =>{
-    return <div className="container-fluid h1 p-5 text-center">Register Page</div>
+import { useState } from "react";
+import RegisterForm from "../components/RegisterForm";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Register = ({history}) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API}/register`, {
+        name: name,
+        email: email,
+        password: password,
+      });
+      console.log("REGISTER USER ===>", res);
+      toast.success("Register success. Please login.");
+      history.push("./login")
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 400) toast.error(err.response.data);
+    }
+  };
+
+  return (
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register Page</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <RegisterForm
+              handleSubmit={handleSubmit}
+              name={name}
+              setName={setName}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
--- server/server.js
+++ server/server.js
@@ -8,14 +8,20 @@ require("dotenv").config();
 const app = express();
 
 //db connection
-console.log(process.env.DATABASE)
+console.log(process.env.DATABASE);
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log("DB connection Err: ", err));
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json())
 
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
